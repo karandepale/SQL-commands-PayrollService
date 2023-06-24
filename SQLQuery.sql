45bb0878b2a0d86eb536a2a1367c49e880ffc95b
“karandepale” <karandepale111@gmail.com>
2023-06-24 19:10:38 +0530

    [Karan]: Retriving data

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -7,4 +7,8 @@ create table employee_payroll(ID int identity(1,1) primary key,name varchar(20),
 
 
 -- Inserting new records in the existing table
-insert into employee_payroll values('Madhur',35000,'2020-02-20'),('manish',38000,'2020-03-19');
+insert into employee_payroll values('Karan',35000,'2020-02-20'),('Krishna',38000,'2020-03-19');
+
+
+--Retriving data from table
+select * from employee_payroll;
