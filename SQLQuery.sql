67f738328f3b03a348fb7181fda40348c061ad2b
“karandepale” <karandepale111@gmail.com>
2023-06-24 19:07:49 +0530

    [Karan]: Inserting records into table

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -4,3 +4,7 @@ use payroll_service;
 
 --UC2-Create table employee_payroll--
 create table employee_payroll(ID int identity(1,1) primary key,name varchar(20),Salary float,StartDate date);
+
+
+-- Inserting new records in the existing table
+insert into employee_payroll values('Madhur',35000,'2020-02-20'),('manish',38000,'2020-03-19');
