abb30cf621aca2b9020745aca54f42c237ab9004
“karandepale” <karandepale111@gmail.com>
2023-06-24 19:04:45 +0530

    [Karan]: Creating Table

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,3 +1,6 @@
 --UC1-> Creating payrollService Database;
 create database payroll_service;
 use payroll_service;
+
+--UC2-Create table employee_payroll--
+create table employee_payroll(ID int identity(1,1) primary key,name varchar(20),Salary float,StartDate date);
